0f76d48bc598286cfd402a03dc78e8e8e0e05128
Jordy Shryock <jordy1611@gmail.com>
2020-07-11 17:41:59 -0600

    Update display recipes and pantry pages

--- src/scripts.js
+++ src/scripts.js
@@ -1,44 +1,49 @@
-const ingredients = require('../data/ingredients')
-const recipes = require('../data/recipes');
-const users = require('../data/users');
-const Recipe = require('./Recipe');
-const User = require('./User');
-const Pantry = require('./Pantry');
+// const ingredientsData = require('../data/ingredients.js');
+// // const ingredientsData = require('../data/ingredients.js');
+// const recipesData = require('../data/recipes');
+// const users = require('../data/users');
+// const Recipe = require('./Recipe');
+// const User = require('./User');
+// const Pantry = require('./Pantry');
 
 console.log('Hello World');
 
-const displayRecipes;
-const userRecipes;
-const shoppingList;
+// const allRecipes;
+// const userRecipes;
+// const shoppingList;
 
 //eventListeners
-window.addEventListener('click', /*function*/clickHandler);
+window.addEventListener('click', clickHandler);
 
 //eventHandlers
 function clickHandler(event) {
   if (event.target.classList.contains('recipes-button')) {
     displayRecipesPage();
-  } else if (event.target.classList.contains('recipes-button')) {
-    displayUsersPage();
-  } 
+  } else if (event.target.classList.contains('my-pantry-button')) {
+    displayPantryPage();
+  }
 }
-
 
 const displayRecipesPage = () => {
-  hideElement('pantry-body');
   displayElement('cards-body');
+  displayElement('my-pantry-button');
+  hideElement('pantry-body');
+  hideElement('recipes-button')
+
 }
 
-const displayUsersPage = () => {
-  hideElement('cards-body');
+const displayPantryPage = () => {
   displayElement('pantry-body');
+  displayElement('recipes-button');
+  hideElement('cards-body');
+  hideElement('my-pantry-button');
 }
 
 function displayElement(className) {
-  document.querySelector('.${ className } ').classList.remove(‘hidden’);
+  document.querySelector(`.${className}`).classList.remove('hidden');
 }
   function hideElement(className) {
-  document.querySelector('.${ className } ').classList.add(‘hidden’);
+  document.querySelector(`.${className}`).classList.add('hidden');
 }
 
 //helper functions
