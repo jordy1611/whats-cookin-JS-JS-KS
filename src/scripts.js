3ca297eec249fbe4bbffa50f16769801ba47c1ac
Jordy Shryock <jordy1611@gmail.com>
2020-07-11 18:54:03 -0600

    Create display recipe cards function to display all recipe cards on load

--- src/scripts.js
+++ src/scripts.js
@@ -5,16 +5,16 @@
 // const Recipe = require('./Recipe');
 // const User = require('./User');
 // const Pantry = require('./Pantry');
-
+const cardsBodySection = document.querySelector('.cards-body'); // see if we can create this in a function
 console.log('Hello World');
 
-const allRecipes = generateRecipes(recipeData);
+// const allRecipes = generateRecipes(recipeData);
 // const userRecipes = generateRecipeCards();
 // const shoppingList;
 
 //eventListeners
 window.addEventListener('click', clickHandler);
-
+window.addEventListener('load', onLoad);
 //eventHandlers
 function clickHandler(event) {
   if (event.target.classList.contains('recipes-button')) {
@@ -29,7 +29,6 @@ const displayRecipesPage = () => {
   displayElement('my-pantry-button');
   hideElement('pantry-body');
   hideElement('recipes-button')
-
 }
 
 const displayPantryPage = () => {
@@ -46,6 +45,37 @@ function displayElement(className) {
   document.querySelector(`.${className}`).classList.add('hidden');
 }
 
+function onLoad() {
+  const allRecipes = generateRecipes(recipeData);
+  displayRecipeCards(allRecipes);
+}
+
+function displayRecipeCards(recipeArray) { //image might have quotes already around it
+  recipeArray.forEach(function(recipe) {
+    const card = `
+    <article class="recipe-card">
+      <div class="hidden-card">
+        blah blah blah blah </br>
+        blah blah blah blah blah </br>
+        blah blah blah blah </br>
+        blah blah blah blah blah </br>
+        blah blah blah blah </br>
+        blah blah blah blah blah </br>
+        blah blah blah blah </br>
+        blah blah blah blah blah </br>
+        blah blah blah blah </br>
+        blah blah blah blah blah </br>
+        blah blah blah blah </br>
+        blah blah blah blah blah </br>
+      </div>
+      <section class="displayed-card">
+        <img class="recipe-img" src=${recipe.image}>
+        <p class="recipe-name">${recipe.name}</p>
+      </section>
+    </article>`;
+    cardsBodySection.insertAdjacentHTML('afterbegin', card);
+  })
+}
 //helper functions
 
 // can be tested - should generate array of all recipes on load
